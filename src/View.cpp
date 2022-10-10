f208f8fcf52a7a76450fd2aa4ca9ea6f01acd61b
Malte Langosz <malte.langosz@dfki.de>
2022-10-10 11:40:59 +0200

    Update copy and paste modkeys

--- src/View.cpp
+++ src/View.cpp
@@ -1223,17 +1223,17 @@ namespace osg_graph_viz {
         if(ea.getKey() == osgGA::GUIEventAdapter::KEY_Escape) {
           return true;
         }
-        if(ea.getKey() == 'D') {
+        if(ea.getKey() == 'D' || (ea.getModKeyMask() & osgGA::GUIEventAdapter::MODKEY_SHIFT || ea.getKey() == 'd')) {
           fprintf(stderr, "duplicate\n");
           duplicateSelection();
           return true;
         }
-        if(ea.getKey() == 'C') {
+        if(ea.getKey() == 'C' || (ea.getModKeyMask() & osgGA::GUIEventAdapter::MODKEY_SHIFT || ea.getKey() == 'c')) {
           fprintf(stderr, "copy\n");
           copySelection();
           return true;
         }
-        if(ea.getKey() == 'V') {
+        if(ea.getKey() == 'V' || (ea.getModKeyMask() & osgGA::GUIEventAdapter::MODKEY_SHIFT || ea.getKey() == 'v')) {
           fprintf(stderr, "paste\n");
           pasteSelection();
           return true;
